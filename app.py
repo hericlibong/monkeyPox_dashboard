509052362e04a7ad181aa47b1a47ea38d8cf9787
ricolajazz <heric.afrimages@gmail.com>
2022-06-23 08:53:09 +0000

    go to tabs

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import  Output, Input
+
+import dash_table
 from ipyslickgrid import show_grid
+
 from markdown import Markdown
 import plotly.express as px
 import plotly.graph_objs as go
@@ -13,11 +16,14 @@ import datetime
 
 data_pox = pd.read_csv('df_monkeyPoxTimeSerie.csv')
 
+## global cases
+global_cases = data_pox[data_pox['Country']=='World']['acc_confirmed'].max()
+
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.COSMO], 
                 meta_tags=[{'name':'viewpoort', 
                             'content':'width=device-width, initial-scale= 1.0'}])
 
-####  Set markdown text ###
+####  markdown text ###
 Markdown_text = '''
 Visualizing the data produced by the 
 [Global.health](https://github.com/globaldothealth/monkeypox) 
@@ -25,12 +31,17 @@ team on the 2022 monkeypox outbreak.
 '''
 
 
-##### Set left controls tools ####
+                     ##### CONTROLS SETTINGS ####
 
 control = dbc.Card([
     html.Div([
         dbc.Label('Overview', className = 'text-uppercase font-weight-bold d-inline-block text-dark' ), 
         dbc.Label('Last Update : ' + str(data_pox['date'].iloc[-1]), className='text-warning d-inline-block font-italic badge', style={'padding-left':'15px'}),
+        
+        ### Global cases
+        dbc.Label('Global Cases :  '+ str(global_cases)),
+        
+        #### multi choice dropdown fig 1
         dcc.Dropdown(id= 'my_dropdown', 
                      placeholder= 'select countries', 
                      options= [{'label':c, 'value':c} for c  in sorted(data_pox.Country.unique())],
@@ -38,19 +49,29 @@ control = dbc.Card([
                      multi = True),
         html.Hr(),
         dbc.Label('World & Country Data Informations',  className= 'text-uppercase, font-weight-bold  p-2 text-left'),
+       
+       # single  choice drop down
         dcc.Dropdown(id='my_dropdown2', className='dropdown open',
             options=[{'label':c, 'value':c} for  c in sorted(data_pox.Country.unique())],
             value='World',
             multi = False),
-        dcc.Graph(id='indicator_confirmed', config = {'displayModeBar': False}),
+        dcc.Graph(id='indicator_confirmed', config = {'displayModeBar': False},  style={'padding-top':'15px'}),
+        dash_table.DataTable(id='datatable', 
+                             style_cell={'font-family':'Arial'},
+                             style_data_conditional=[{'if' :{'column_id':'labels'},
+                                                      'textAlign':'left'}] + [{
+                                                          'if':{'row_index':'odd'},
+                                                          'backgroundColor': 'rgb(248, 248, 248)'
+                                                          }], style_header={
+                                                              'backgroundColor': 'rgb(230, 230,  230)',
+                                                              'fontWeight':'bold',
+                                                              'textAlign':'left'}),
         
-        html.P('Global Cases :  '+ str(data_pox[data_pox['Country']=='World']['acc_confirmed'].max())),
-        html.P('New Cases : '+ str(data_pox[data_pox['Country']=='World']['new_daily_cases'].iloc[-1]), 
-               style = {'display':'inline-block'}),
-        #dcc.Graph(id = 'indic'),
         html.Hr()
     ])
 ], body=True, style = {'padding-top': '15px', 'verticalAlign':'top'})
+
+             ### LAYOUT SETTINGS #####  
 
 app.layout = dbc.Container([
     
@@ -97,8 +118,48 @@ def update_ligneGraph(value):
 
 @app.callback(Output('indicator_confirmed', 'figure'), [Input('my_dropdown2', 'value')])
 def  update_indicator(value):
-    value_confirmed = data_pox[data_pox['Country'].isin(value)]['acc_confirmed'].iloc[-1]- data_pox[data_pox['Country'].isin(value)]['acc_confirmed'].iloc[-2]
-    delta_confirmed = data_pox[data_pox['Country'].isin(value)]['acc_confirmed'].iloc[-2]- data_pox[data_pox['Country'].isin(value)]['acc_confirmed'].iloc[-3]
+    value_confirmed = data_pox[data_pox['Country']==value]['acc_confirmed'].iloc[-1] - data_pox[data_pox['Country']==value]['acc_confirmed'].iloc[-2]
+    delta_confirmed = data_pox[data_pox['Country']==value]['acc_confirmed'].iloc[-2] - data_pox[data_pox['Country']==value]['acc_confirmed'].iloc[-3]
+    return {
+        'data': [go.Indicator(
+            mode = 'number+delta',
+            value = value_confirmed,
+            delta = {'reference': delta_confirmed,
+                     'position':'right',
+                     'valueformat': ',g',
+                     'relative':False,
+                     'font':{'size':15}},
+            number= {'valueformat':',',
+                     'font':{'size':10}},
+            domain = {'y': [1, 0.5], 'x':  [1, 0.5]}
+            
+        )],
+        
+        'layout': go.Layout(
+            title = {'text': 'Count', 
+                     'y': 1,
+                   'x': 0.5,
+                   'xanchor': 'right',
+                   'yanchor': 'top'}, 
+            font = dict(color='blue'),
+            height= 50
+    
+        )
+    }
+    
+@app.callback([Output('datatable', 'data'), Output('datatable', 'columns')], [Input('my_dropdown2', 'value')])
+def get_table(value):
+    table_df = data_pox.groupby(['Country']).agg(New=('new_daily_cases', 'last'), Total =('acc_confirmed', 'max')).reset_index()
+    columns = ['New', 'Total']
+    infos  = {
+        'labels': columns,
+        'data': [table_df[table_df['Country']==value][col].iloc[0] for col in columns]
+        }
+    table = pd.DataFrame(infos)
+    data = table.to_dict('rows')
+    header =[ {'id':'labels',  'name':'Cases'}, {'id':'data', 'name':'Total'}]
+    return data, header
+    
     
     
 
